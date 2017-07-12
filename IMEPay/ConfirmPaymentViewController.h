1bec18b875826e6978e31793927ce94040d197af
Manoj Karki <manojkarkie@gmail.com>
2017-07-12 15:18:20 +0545

    UI Work

---
+++ IMEPay/ConfirmPaymentViewController.h
@@ -0,0 +1,13 @@
+//
+//  ConfirmPaymentViewController.h
+//  IMEPay
+//
+//  Created by Manoj Karki on 7/12/17.
+//  Copyright © 2017 Manoj Karki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ConfirmPaymentViewController : UIViewController
+
+@end
