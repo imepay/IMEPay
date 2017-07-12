23b53770aa91df9e7fecb191f349fa673833ada7
Manoj Karki <manojkarkie@gmail.com>
2017-07-12 09:52:27 +0545

    Initial Commit

---
+++ IMEPay/IMEPay.h
@@ -0,0 +1,19 @@
+//
+//  IMEPay.h
+//  IMEPay
+//
+//  Created by Manoj Karki on 7/12/17.
+//  Copyright © 2017 Manoj Karki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IMEPay.
+FOUNDATION_EXPORT double IMEPayVersionNumber;
+
+//! Project version string for IMEPay.
+FOUNDATION_EXPORT const unsigned char IMEPayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IMEPay/PublicHeader.h>
+
+
