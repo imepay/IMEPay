f4ef74adae54a067b3eae6274bf2154317cb1a7d
Manoj Karki <manojkarkie@gmail.com>
2017-12-31 16:06:45 +0545

    minor changes

--- IMEPay/IMPPaymentManager.h
+++ IMEPay/IMPPaymentManager.h
@@ -13,6 +13,6 @@
 
 - (instancetype)initWithEnvironment:(APIEnvironment)environment;
 
-- (void)pay:(NSString *)userName password:(NSString *)password  merchantCode:(NSString *)merchantCode merchantName: (NSString *)merchantName  merchantUrl:(NSString *)merchantUrl amount:(NSString *)amount customerMobileNumber:(NSString *)customerMobileNumber referenceId: (NSString *)referenceId module: (NSString *)module success: (void(^)(NSDictionary *transactionInfo))success failure: (void(^)(NSDictionary *transactionInfo))failure;
+- (void)pay:(NSString *)userName password:(NSString *)password  merchantCode:(NSString *)merchantCode merchantName: (NSString *)merchantName  merchantUrl:(NSString *)merchantUrl amount:(NSString *)amount customerMobileNumber:(NSString *)customerMobileNumber referenceId: (NSString *)referenceId module: (NSString *)module success: (void(^)(NSDictionary *transactionInfo))success failure: (void(^)(NSDictionary *transactionInfo))failure;    
 
 @end
